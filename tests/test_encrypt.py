823c5bf1056e82ffdc7f93c8ae7e2f8473b83ff9
ElectricErudite <ElectricErudite@protonmail.com>
2019-11-30 15:34:04 -0500

    Add pytest for encryption suite

---
+++ tests/test_encrypt.py
@@ -0,0 +1,115 @@
+#########################################################################
+# pytest -v --capture=no tests/test_encryption.py
+# pytest -v  tests/test_encryption.py
+# pytest -v --capture=no  tests/test_encryption.py:Test_name.<METHIDNAME>
+#########################################################################
+""" run with
+
+pytest -v --capture=no tests/test_encryption.py
+
+"""
+
+import os
+import pytest
+import tempfile
+from cloudmesh.configuration.Config import Config
+from cloudmesh.common.util import path_expand, writefile, readfile
+from cloudmesh.security.encrypt import KeyHandler, CmsEncryptor
+
+@pytest.mark.incremental
+class TestEncrypt:
+
+    def test_sec_section_exists(self):
+        config = Config()
+        res = config['cloudmesh.security']
+        assert (res != [])
+
+    def test_kh_load_public(self):
+        # Generate new keys
+        kh = KeyHandler()
+        r = kh.new_rsa_key(ask_password=False)
+        u = kh.get_pub_key_bytes()
+
+        # Write keys to temp
+        tmp = tempfile.NamedTemporaryFile(delete=True)
+        tmp.write(u)
+        tmp.seek(0)
+
+        # Load the keys
+        tu = kh.load_key(tmp.name, key_type="PUB", encoding="PEM", ask_pass=False)
+
+        # Serialize the bytes
+        t = kh.serialize_key(key=tu, key_type="PUB"
+                        , encoding="PEM", format="SubjectInfo")
+        #Check if they are the same
+        assert(t == u)
+
+    def test_kh_load_private(self):
+        # Generate new keys
+        kh = KeyHandler()
+        r = kh.new_rsa_key(ask_password=False)
+
+        # Write keys to temp
+        tmp = tempfile.NamedTemporaryFile(delete=True)
+        tmp.write(r)
+        tmp.seek(0)
+
+        # Load the keys
+        tr = kh.load_key(tmp.name, key_type="PRIV", encoding="PEM", ask_pass=False)
+
+        # Serialize the bytes
+        t = kh.serialize_key(key=tr, key_type="PRIV"
+                        , encoding="PEM", format="PKCS8")
+
+        #Check if they are the same
+        assert(t == r)
+
+    def test_RSA_encryption(self):
+        ce = CmsEncryptor()
+        data = ce.getRandomBytes()
+
+        # Generate new keys
+        kh = KeyHandler()
+        r = kh.new_rsa_key(ask_password=False)
+        u = kh.get_pub_key_bytes()
+
+        # Load the public key
+        ## Since 'u' is currently in bytes form we need to reload for pyca key
+        tmp = tempfile.NamedTemporaryFile(delete=True)
+        tmp.write(u)
+        tmp.seek(0)
+        u = kh.load_key(tmp.name, key_type="PUB", encoding="PEM", ask_pass=False)
+
+        # Load priv key
+        ## Since 'r' is currently in bytes form we need to reload for pyca key
+        tmp.write(r)
+        tmp.seek(0)
+        r = kh.load_key(tmp.name, key_type="PRIV", encoding="PEM", ask_pass=False)
+
+        # Encrypt the data
+        ct = ce.encrypt_rsa(pub=u, pt=data, padding_scheme="OAEP")
+
+        # Decrypt the data
+        pt = ce.decrypt_rsa(priv=r, ct = ct, padding_scheme="OAEP")
+
+        # Ensure the data is equivalent
+        assert(data == pt)
+
+    def test_AESGCM_nonces(self):
+        # Given the same data and aad will the nonces be equal?
+        ce = CmsEncryptor()
+        data = ce.getRandomBytes()
+        aad = b"test"
+        # Given two callings on cipher the nonce should never repeat
+        k1, n1, ct1 =ce.encrypt_aesgcm(data=data, aad=aad)
+        k2, n2, ct2 = ce.encrypt_aesgcm(data=data, aad=aad)
+        assert(n1 != n2)
+
+    def test_AESGCM_encryption(self):
+        # Will the encryption and decryption be equivalent?
+        ce = CmsEncryptor()
+        data = ce.getRandomBytes()
+        aad = b"test"
+        k, n, ct = ce.encrypt_aesgcm(data=data, aad=aad)
+        pt = ce.decrypt_aesgcm(key=k, nonce=n, aad=aad, ct=ct)
+        assert (pt == data)
