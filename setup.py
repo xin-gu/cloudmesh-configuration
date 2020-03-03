75a994e3cba37e043717d9badf0db177fa27efec
Gregor von Laszewski <laszewski@gmail.com>
2020-03-03 01:26:04 -0500

    add package data

--- setup.py
+++ setup.py
@@ -63,6 +63,9 @@ setup(
     url=URL,
     packages=find_packages(exclude=("tests")),
     include_package_data=True,
+    package_data={
+        "": ["*.yaml"],
+    },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
