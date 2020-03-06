a0893e749de86302ea6b5439e9c520ea748a7525
Gregor von Laszewski <laszewski@gmail.com>
2020-03-06 15:42:52 -0500

    remove deprecated

--- deprecated/config.py
+++
@@ -1,7 +0,0 @@
-import six
-
-if six.PY2:
-    import cloudmesh.confif.v2
-    from cloudmesh.config.v2.config import Config, Active
-else:
-    from cloudmesh.config.v3.config import Config, Active
